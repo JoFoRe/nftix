3534bd58a064f960605bb04ea5de35adb67a30a8
JaFoRe <jfocke13@gmail.com>
2022-02-28 23:26:57 +0100

    config.js hidden

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const nftmarketaddress = "0x60d8c6cBfc0f4ae0403B1843Ff048c61Fea40463"
-  export const nftaddress = "0x00772E585e70b91D77C3d5f02b4B8752b344B07A"
+  export const nftmarketaddress = "0xc761021883B5542B51dA4055165C777462dfb3D6"
+  export const nftaddress = "0x898Fe82410DdC60B89DFfcE115D1438B7FE8feb9"
   
